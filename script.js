f97b4c7aa666e6be7dacffd3fb84dd1fe0ed34b5
guilherezardo <guilhermezardo0@gmail.com>
2023-07-20 21:57:39 -0300

    final mesmo

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 const result=document.querySelector(".result");
 const player=document.querySelector(".player-score");
 const aiScore=document.querySelector(".ai-score");
@@ -55,58 +53,94 @@ function playRound(playerSelection, computerSelection) {
         player.textContent="player score:" + playerScore;
         aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="you lose! paper beats rock."
+        if (ComputerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Game over! You lost.")
+        }
         return "you lose! paper beats rock."
     }
     else if (p=="rock" && c=="scissors"){
         playerScore++;
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="you win! rock beats scissors."
+        if (playerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Congratulations! You won!")
+        }
         return "you win! rock beats scissors."
     }
     else if (p=="rock" && c=="rock"){
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="It's a draw.";
         return "It's a draw."
     }
     else if (p=="paper" && c=="rock"){
         playerScore++;
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent= "you win! paper beats rock.";
+        if (playerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Congratulations! You won!")
+        }
         return "you win! paper beats rock."
     }
     else if (p=="paper" && c=="scissors"){
         ComputerScore++;
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="you lose! scissors beats paper."
+        if (ComputerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Game over! You lost.")
+        }
         return "you lose! scissors beats paper."
     }
     else if (p=="paper" && c=="paper"){
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="It's a draw."
         return "It's a draw."
     }
     else if (p=="scissors" && c=="paper"){
         playerScore++;
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="you win! scissors beats paper."
+        if (playerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Congratulations! You won!")
+        }
         return "you win! scissors beats paper."
     }
     else if (p=="scissors" && c=="rock"){
         ComputerScore++;
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="you lose! rocks beats scissors"
+        if (ComputerScore==5){
+            rock.disabled=true;
+            paper.disabled=true;
+            scissor.disabled=true;
+            return alert("Game over! You lost.")
+        }
         return "you lose! rocks beats scissors"
     }
     else if (p=="scissors" && c=="scissors"){
-        player.textContent=playerScore;
-        aiScore.textContent=ComputerScore;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
         result.textContent="Its a draw."
         return "Its a draw."
     }
