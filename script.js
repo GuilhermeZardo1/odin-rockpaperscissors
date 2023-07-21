3a07e472c96ea042a6c72f85fbb2302f27c84dc8
guilherezardo <guilhermezardo0@gmail.com>
2023-07-20 21:40:34 -0300

    eventos dos botões acrescentados. Div's criados. Lógica interna sendo trabalhada.

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-console.log("Hello World");
+
+
+
+const result=document.querySelector(".result");
+const player=document.querySelector(".player-score");
+const aiScore=document.querySelector(".ai-score");
+
+player.textContent=playerScore;
+aiScore.textContent=ComputerScore;
+
+const rock = document.querySelector('#rock');
+
+rock.addEventListener('click', ()=> {
+    console.log(playRound("rock", getComputerChoice()))
+});
+
+const paper = document.querySelector('#paper');
+
+paper.addEventListener('click', ()=> {
+    console.log(playRound("paper", getComputerChoice()))
+});
+
+const scissor = document.querySelector('#scissor');
+
+scissor.addEventListener('click', ()=> {
+    console.log(playRound("scissors", getComputerChoice()))
+});
 
 function getComputerChoice(){
 
     let randomNumber = Math.floor(Math.random()*3);
     if (randomNumber==0){
+        player.textContent=playerScore;
+
         return "rock";
     }
     else if (randomNumber==1){
@@ -24,52 +52,68 @@ function playRound(playerSelection, computerSelection) {
 
     if (p=="rock" && c=="paper"){
         ComputerScore++;
+        player.textContent="player score:" + playerScore;
+        aiScore.textContent="computer score:" + ComputerScore;
+        result.textContent="you lose! paper beats rock."
         return "you lose! paper beats rock."
     }
     else if (p=="rock" && c=="scissors"){
         playerScore++;
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="you win! rock beats scissors."
         return "you win! rock beats scissors."
     }
     else if (p=="rock" && c=="rock"){
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="It's a draw.";
         return "It's a draw."
     }
     else if (p=="paper" && c=="rock"){
         playerScore++;
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent= "you win! paper beats rock.";
         return "you win! paper beats rock."
     }
     else if (p=="paper" && c=="scissors"){
         ComputerScore++;
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="you lose! scissors beats paper."
         return "you lose! scissors beats paper."
     }
     else if (p=="paper" && c=="paper"){
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="It's a draw."
         return "It's a draw."
     }
     else if (p=="scissors" && c=="paper"){
         playerScore++;
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="you win! scissors beats paper."
         return "you win! scissors beats paper."
     }
     else if (p=="scissors" && c=="rock"){
         ComputerScore++;
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="you lose! rocks beats scissors"
         return "you lose! rocks beats scissors"
     }
     else if (p=="scissors" && c=="scissors"){
+        player.textContent=playerScore;
+        aiScore.textContent=ComputerScore;
+        result.textContent="Its a draw."
         return "Its a draw."
     }
 }
-   
-    function game(){
-        for (let i = 0; i < 5; i++) {
-            let c=getComputerChoice();
-            let p=prompt("choose rock, paper or scissor.")
-            p=p.toLowerCase();
-            
-            playRound(p, c);
 
-            
-        }
-
-          console.log("the final result is: player:" + playerScore + "and computer:" + ComputerScore);
-    }
+    function game(){}
+      
 
     game();
 
